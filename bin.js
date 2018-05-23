fe7efe7b3b54a72e759a4d66f9636b682d026877
Elvey <Matthew.Elvey@cdu.edu.au>
2018-05-23 17:59:09 +0930

    Fix executable

---
+++ bin.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+var minimist = require('minimist');
+var validateGitlabCi = require('./validate-gitlab-ci');
+
+var args = minimist(process.argv);
+
+function showUsage() {
+	console.log(
+		'usage validate-gitlab-ci --server <gitlab server> --filename <filename>\n'
+		+ 'arguments:\n'
+		+ '	--filename 		Path to .gitlab-ci file to validate\n'
+		+ '	--server: 		Base URL for the gitlab server to validate against'
+	);
+}
+
+if(!args.filename) {
+	showUsage();
+	process.exit(-1);
+};
+
+validateGitlabCi(args.server, args.filename).then(() => {
+	process.exit();
+});
--- validate-gitlab-ci.js
+++ validate-gitlab-ci.js
@@ -1,33 +1,15 @@
 var https = require('https');
 var fs = require('fs');
-var minimist = require('minimist');
 var fetch = require('node-fetch');
 var yaml = require('js-yaml');
 
 var endpointPath = '/api/v4/ci/lint';
 
-const args = minimist(process.argv);
-
-function showUsage() {
-	console.log(
-		'usage validate-gitlab-ci --server <gitlab server> --filename <filename>\n'
-		+ 'arguments:\n'
-		+ '	--filename 		Path to .gitlab-ci file to validate\n'
-		+ '	--server: 		Base URL for the gitlab server to validate against'
-	);
-}
-
-if(!args.filename) {
-	showUsage();
-	return -1;
-};
-
-fs.readFile(args.filename, (err, text) => {
-	console.log(text.toString());
+function validateGitlabCi(server, filename) {
+	var text = fs.readFileSync(filename);
 	var yamlDoc = yaml.safeLoad(text.toString());
 	var yamlEncoded = JSON.stringify(yamlDoc);
-	console.log(JSON.stringify({content: yamlEncoded}));
-	return fetch(args.server + endpointPath, {
+	return fetch(server + endpointPath, {
 		method: 'POST',
 		headers: {
 			'content-type': 'application/json'
@@ -37,4 +19,6 @@ fs.readFile(args.filename, (err, text) => {
 		.then((response) => response.json())
 		.then((response) => console.log(response))
 		.catch((err) => console.error(err));
-});
+}
+
+module.exports = validateGitlabCi;
